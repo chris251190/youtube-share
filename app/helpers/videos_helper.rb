da81c330d54f4ab99c8629f556a148671c14b06a
christian_lehr.1@gmx.de <christian_lehr.1@gmx.de>
2015-12-29 00:19:11 +0100

    Added https for youtube_embed method because heroku only allows https

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -9,7 +9,7 @@ module VideosHelper
     youtube_id = $5
   end
 
-  %Q{<iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+  %Q{<iframe title="YouTube video player" width="640" height="390" src="https://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
 end
 
 end
