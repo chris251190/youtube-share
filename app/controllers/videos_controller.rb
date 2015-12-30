ca263e18f66d40d06c8ebf3f4f9294028eeb1597
christian_lehr.1@gmx.de <christian_lehr.1@gmx.de>
2015-12-30 23:11:17 +0100

    Added user permissions and adjusted icons

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -17,10 +17,11 @@ class VideosController < ApplicationController
 	end
 
 	def edit
-		if(current_user.id == Video.find(params[:id]).user_id) 
-			@video = Video.find(params[:id]) 
+		@video = Video.find(params[:id])
+		if(current_user.id == @video.user_id) 
+			 
 		else
-			redirect_to root_path
+			redirect_to @video
 		end
 	end	
 
@@ -37,17 +38,22 @@ class VideosController < ApplicationController
 	def update
 		@video = Video.find(params[:id])
 		if @video.update(video_params)
-			redirect_to @video
+			redirect_to root_path
 		else
 			render 'edit'
 		end
 	end
 
-	def destroy
+	def destroy		
+
+	if(current_user.id == Video.find(params[:id]))
 		@video = Video.find(params[:id])
 		@video.destroy
-
-		redirect_to videos_path
+		redirect_to root_path
+	else
+		redirect_to root_path
+	end
+		
 	end
 
 	def my_videos
