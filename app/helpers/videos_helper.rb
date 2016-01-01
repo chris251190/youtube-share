550ca8230d5219a59846729ae034001cb77ddd7d
christian_lehr.1@gmx.de <christian_lehr.1@gmx.de>
2016-01-01 17:27:51 +0100

    added helper method for getting random video

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,15 +1,19 @@
 module VideosHelper
 
 	def youtube_embed(youtube_url)
-  if youtube_url[/youtu\.be\/([^\?]*)/]
-    youtube_id = $1
-  else
-    # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
-    youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
-    youtube_id = $5
-  end
+		if youtube_url[/youtu\.be\/([^\?]*)/]
+			youtube_id = $1
+		else    	
+    		youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+    		youtube_id = $5
+		end	
 
-  %Q{<iframe class="iframe-resize" title="YouTube video player" width="100%" height="390px" src="https://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
-end
+	%Q{<iframe class="iframe-resize" title="YouTube video player" width="100%" height="390px" src="https://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+	end
+
+
+	def get_random_video_from_public
+		Video.all.where(public: true).order("Random()").first(5)		
+	end
 
 end
