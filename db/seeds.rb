09ae7edf210f1ca418d33d2e4b3d706288520c2a
christian_lehr.1@gmx.de <christian_lehr.1@gmx.de>
2016-01-01 16:38:55 +0100

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-videos = Video.create([{ title: "'The Journey' (2 Hour Drum & Bass Mix)", url: "https://www.youtube.com/watch?v=R8MWKsheHxk", text: "", public: true},
+videos = Video.create([{ title: "The Journey (2 Hour Drum & Bass Mix)", url: "https://www.youtube.com/watch?v=R8MWKsheHxk", text: "", public: true},
 					   { title: "Meiko - Leave The Lights On (Krot Remix)", url: "https://www.youtube.com/watch?v=uO7kCUjUaUE", text: "", public: true},
 					   { title: "Marina And The Diamonds - Lies (Zeds Dead Remix)", url: "https://www.youtube.com/watch?v=KTYD_kN7rPg", text: "", public: true}])
